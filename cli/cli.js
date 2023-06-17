1b9219f4d108995667da08b40e3e1bb90c78b4a7
ljacobsson <lars@mathem.se>
2023-06-17 14:47:38 +0200

    fixing cli setup for js

--- cli/cli.js
+++ cli/cli.js
@@ -4,24 +4,25 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf-8'));
-if (!tsconfig.compilerOptions.outDir) {
-  tsconfig.compilerOptions.outDir = "dist";
+let outDir = null;
+if (fs.existsSync("tsconfig.json")) {
+  const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf-8'));
+  if (!tsconfig.compilerOptions.outDir) {
+    tsconfig.compilerOptions.outDir = "dist";
+  }
+  outDir = tsconfig.compilerOptions.outDir;
+  tsconfig.compilerOptions.resolveJsonModule = true;
+  tsconfig.compilerOptions.sourceMap = true;
+  tsconfig.compilerOptions.noEmit = false;
+  fs.writeFileSync("tsconfig.json", JSON.stringify(tsconfig, null, 2));
 }
-const outDir = tsconfig.compilerOptions.outDir;
-tsconfig.compilerOptions.resolveJsonModule = true;
-tsconfig.compilerOptions.sourceMap = true;
-tsconfig.compilerOptions.noEmit = false;
-fs.writeFileSync("tsconfig.json", JSON.stringify(tsconfig, null, 2));
-
 let ide = process.argv[2];
 if (!ide) {
   ide = process.env.TERM_PROGRAM;
 }
 try {
-const ideScript = await import(`file://${__dirname}/${ide}.js`);
-ideScript.configure(outDir);
+  const ideScript = await import(`file://${__dirname}/${ide}.js`);
+  ideScript.configure(outDir);
 } catch (e) {
   console.error(`${ide} is not supported`);
   process.exit(1);
--- cli/vscode.js
+++ cli/vscode.js
@@ -22,24 +22,25 @@ export function configure(outDir) {
   launchConfigStr = launchConfigStr.replace(/\/\/.*/g, '');
   const launchConfig = JSON.parse(launchConfigStr);
   const launchConfigExists = launchConfig.configurations.find((config) => config.name === "Debug Lambda functions");
-  if (!launchConfigExists) launchConfig.configurations.push(
-    {
-      "type": "node",
-      "request": "launch",
-      "name": "Debug Lambda functions",
-      "runtimeExecutable": "${workspaceFolder}/node_modules/.bin/nodemon",
-      "skipFiles": ["<node_internals>/**"],
-      "program": "${workspaceFolder}/node_modules/lambda-debug/connect.js",
-      "restart": true,
-      "env": {
-        "outDir": outDir
-      },
-      "postDebugTask": "lambda-debug-cleanup",
-      "console": "integratedTerminal",
-      "internalConsoleOptions": "neverOpen"
+  const config = {
+    "type": "node",
+    "request": "launch",
+    "name": "Debug Lambda functions",
+    "runtimeExecutable": "${workspaceFolder}/node_modules/.bin/nodemon",
+    "skipFiles": ["<node_internals>/**"],
+    "program": "${workspaceFolder}/node_modules/lambda-debug/connect.js",
+    "restart": true,
+    "env": {
     },
-  );
+    "postDebugTask": "lambda-debug-cleanup",
+    "console": "integratedTerminal",
+    "internalConsoleOptions": "neverOpen"
+  };
+  if (outDir) {
+    config.env.outDir = outDir;
+  }
 
+  if (!launchConfigExists) launchConfig.configurations.push(config);
   fs.writeFileSync(".vscode/launch.json", JSON.stringify(launchConfig, null, 2));
 
   let tasksConfigStr = fs.readFileSync(".vscode/tasks.json", 'utf-8');
